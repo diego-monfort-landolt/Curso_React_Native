02380eb66df1af63fe780c13b855a2e7280e2fcd
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-08-25 22:54:28 +0200

    segundo commit - probando añadir imagenes y estilos en las imagenes

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native'; // <-- Image hinzugefügt
+
+const splash = require('./assets/compt2.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi Diego!</Text>
+      <Text>Esto es un test...</Text>
+      <Image source={splash} 
+      style={{ 
+        width: 100, 
+        height: 100, 
+        borderRadius: 10,
+        boxShadow: '#000',
+        marginTop: 20,
+        }} 
+        />
       <StatusBar style="auto" />
     </View>
   );
