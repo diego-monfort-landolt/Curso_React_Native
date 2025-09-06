fd54927f1e832a482168090ddae364007d6b871e
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-09-06 23:38:38 +0200

    adjuste codigo quite espacios vacios

--- react-native-app/components/Main.jsx
+++ react-native-app/components/Main.jsx
@@ -17,23 +17,16 @@ export default function Main({ g, index }) {
 
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
-
       <Link href='/about' asChild>
         <Pressable className="absolute top-0 left-4 p-2 rounded-lg bg-black/60">
           <AntDesign name="user" size={24} color="white" />
         </Pressable>
       </Link>
-
       <View style={{ height: 20, margin: 2 }}>
-
         <View className="absolute top-5 left-4 bg-black/60 p-2 rounded-lg ">
           <Logo width={48} height={48} />
         </View>
-
       </View>
-
-
-
       {game.length === 0 ? (
         <ActivityIndicator color={'red'} size={'large'} />) : (
         <FlatList
