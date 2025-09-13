466e8ac48d37605fbcb7e0f243ade9ecdafe9082
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-09-14 00:22:55 +0200

    hinzufügen bilder etc - und styles

--- react-native-app/app/about.js
+++ react-native-app/app/about.js
@@ -1,4 +1,10 @@
-import { Text, ScrollView, Pressable } from "react-native"
+import {
+  Text,
+  ScrollView,
+  Pressable,
+  Image,
+  TouchableOpacity,
+} from "react-native"
 import { Link } from "expo-router"
 import { HomeIcon } from "../components/Icons"
 
@@ -21,42 +27,92 @@ export default function About() {
         Sobre el proyecto
       </Text>
 
-      <Text className="text-white font-bold mb-6 text-2xl">
+      <Text className="text-white font-bold mb-6 text-current">
         This is a simple app to show free games using React Native and Expo.
         lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis lorem
       </Text>
 
-      <Text className="text-white font-bold mb-6 text-2xl">
+      <Text className="text-white font-bold mb-6 text-current">
+        This is a simple app to show free games using React Native and Expo.
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis lorem
+      </Text>
+      <TouchableOpacity
+        onPress={() =>
+          Linking.openURL("https://pixabay.com/de/images/search/strand/")
+        }
+        className="mb-6"
+      >
+        <Image
+          source={{
+            uri: "https://cdn.pixabay.com/photo/2022/04/16/01/36/woman-7135489_640.jpg",
+          }}
+          className="w-full h-screen object-cover rounded-lg"
+        />
+      </TouchableOpacity>
+
+      <Text className="text-white font-bold mb-6 text-1xl">
+        This is a simple app to show free games using React Native and Expo.
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis lorem
+      </Text>
+      <Text className="text-white font-bold mb-6 text-1xl">
+        This is a simple app to show free games using React Native and Expo.
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis lorem
+      </Text>
+      <Text className="text-white font-bold mb-6 text-1xl">
+        This is a simple app to show free games using React Native and Expo.
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis lorem
+      </Text>
+      <Text className="text-white font-bold mb-6 text-1xl">
+        This is a simple app to show free games using React Native and Expo.
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis lorem
+      </Text>
+      <Image
+        source={{
+          uri: "https://cdn.pixabay.com/photo/2023/01/23/17/38/seagull-7739257_1280.jpg",
+        }}
+        className="w-full h-screen object-cover bottom-0 mb-6 rounded-lg"
+      />
+
+      <Text className="text-white font-bold mb-6 text-1xl">
+        This is a simple app to show free games using React Native and Expo.
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis lorem
+      </Text>
+      <Image
+        source={{
+          uri: "https://cdn.pixabay.com/photo/2014/03/24/10/17/beach-293826_1280.jpg",
+        }}
+        className="w-full h-screen object-cover bottom-0 mb-6 rounded-lg"
+      />
+
+      <Text className="text-white font-bold mb-6 text-current">
         This is a simple app to show free games using React Native and Expo.
         lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis lorem
       </Text>
 
-      <Text className="text-white font-bold mb-6 text-2xl">
+      <Text className="text-white font-bold mb-6 text-current">
         This is a simple app to show free games using React Native and Expo.
         lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis lorem
       </Text>
 
-      <Text className="text-white font-bold mb-6 text-2xl">
-        This is a simple app to show free games using React Native and Expo.
-        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis lorem
-      </Text>
-
-      <Text className="text-white font-bold mb-6 text-2xl">
-        This is a simple app to show free games using React Native and Expo.
-        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis lorem
-      </Text>
-
-      <Text className="text-white font-bold mb-6 text-2xl">
+      <Text className="text-white font-bold mb-6 text-current">
         This is a simple app to show free games using React Native and Expo.
         lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
